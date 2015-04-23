55d8a0546c6104f56206018d7187d10144d74032
darrenwong23 <darrenwong23@gmail.com>
2015-04-22 20:41:30 -0700

    did some work

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,18 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, $location, Links) {
+  $scope.data = {};
+
+
+  $scope.getLinks = function(){
+    $http.get('/api/links').
+      success(function(data, status, headers, config) {
+        $scope.data.links = data;
+        console.log(data);
+
+      });
+    };
+
+  $scope.getLinks();
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,15 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, $http, Links) {
   // Your code here
+  $scope.url = {
+        link: 'http://www.google.com'
+      };
+
+  $scope.addLink = function(link){
+    console.log(link);
+
+    $http.post("/api/links", {status:201, url:link});
+    $scope.url.link = '';
+  };
 });
